2ba16aba01a4a299c7a3bf124e6fb3a55e1eca2c
Elissa <elissa.matsushita@gmail.com>
2020-01-16 23:10:28 +0000

    mocha and chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,7 +6,11 @@ const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
 
-    callback(error, data[0].description);
+    if (data.length === 0) {
+      callback(error, null);
+    } else {
+      callback(error, data[0].description);
+    }
   }
   );
 };
