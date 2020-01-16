c11bb0343621b60498164facf08d8bbd1bd2914c
Elissa <elissa.matsushita@gmail.com>
2020-01-16 22:45:38 +0000

    added index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,14 @@
 const request = require('request');
-const args = process.argv.splice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  const data = JSON.parse(body);
 
-  if (body === "[]") {
-    return console.log("Error: Breed not found");
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    callback(error, data[0].description);
   }
+  );
+};
 
-  if (response.statusCode < 200 || response.statusCode > 203) {
-    return console.log(response && response.statusCode);
-  }
-
-  if (error) {
-    console.log("Error: ", error);
-  }
-
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
